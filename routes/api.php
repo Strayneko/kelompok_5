3296c852346059fcd91bba33c33b832d7af69e46
Nora Neko <bayurendi10@gmail.com>
2022-12-24 09:35:53 +0700

    (+) add api base endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AspirationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// aspiration route grouping
+Route::name('aspiration.')
+    ->prefix('aspiration')
+    ->controller(AspirationController::class)
+    ->group(function () {
+        Route::get('/', 'index');
+        Route::get('/{id}', 'show');
+        Route::get('/{id}/edit', 'edit');
+        Route::post('/{id}/update', 'update');
+        Route::post('/create', 'store');
+        Route::delete('/{id}/delete', 'destroy');
+    });
+
+// auth route grouping
+Route::name('auth.')
+    ->prefix('auth')
+    ->controller(AspirationController::class)
+    ->group(function () {
+        Route::get('/', 'login');
+        Route::get('/register', 'register');
+        Route::post('/', 'authenticate');
+        Route::post('/register', 'registration');
+    });
