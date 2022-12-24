d583ff30420474bf4749c51acfb979b9ea6b52c6
tegarp00 <tegarp00@gmail.com>
2022-12-24 13:19:50 +0700

    changes endpoint makeAdmin

--- app/Http/Controllers/Backend/AspirationController.php
+++ app/Http/Controllers/Backend/AspirationController.php
@@ -130,29 +130,5 @@ class AspirationController extends Controller
         ]);
     }
 
-    public function makeAdmin($id)
-    {
-        $user = User::find($id);
-        if(!$user) {
-            return response()->json([
-                'status_code' => 404,
-                'status' => false,
-                'message' => "ID User tidak ditemukan",
-                'data' => []
-            ]);
-        }
 
-        $payload = [
-            'role_id' => 2
-        ];
-        
-        $user->update($payload);
-
-       return response()->json([
-            'status_code' => 200,
-            'status' => true,
-            'message' => "User mendapatkan hak akses Admin",
-            'data' => $user
-       ]);
-    }
 }
