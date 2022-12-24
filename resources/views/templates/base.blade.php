1bc12ef2a6367c44c31c43c47d4e429f1fee5f97
Nora Neko <bayurendi10@gmail.com>
2022-12-24 08:08:27 +0700

    :zap: add container to main content

--- resources/views/templates/base.blade.php
+++ resources/views/templates/base.blade.php
@@ -14,9 +14,11 @@
     {{-- import navbar component --}}
     <x-navbar />
 
-    <main>
-        @yield('content')
-    </main>
+    <div class="container">
+        <main>
+            @yield('content')
+        </main>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
