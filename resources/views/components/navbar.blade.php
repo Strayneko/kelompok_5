e8ca53f677c327939ee88a8d7d9eaddb4e3be9bb
Nora Neko <bayurendi10@gmail.com>
2022-12-24 08:02:59 +0700

    :heavy_plus_sign: add bootstrap, and setup navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Group 5</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+        </div>
+    </div>
+</nav>
