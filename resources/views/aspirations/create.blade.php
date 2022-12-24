c8c9dadf083d7ca2a7bf3e2993a05c2a31867524
Nora Neko <bayurendi10@gmail.com>
2022-12-24 10:20:15 +0700

    add form

--- resources/views/aspirations/create.blade.php
+++ resources/views/aspirations/create.blade.php
@@ -16,21 +16,8 @@
          </div>
 
          <div class="mb-3">
-             <label for="content_response" class="form-label">Isi Aspirasi</label>
-             <textarea name="content_response" class="form-control" id="content_response" cols="30" rows="10">{{ old('content_response') }}</textarea>
-         </div>
-
-         <div class="mb-3">
-             <label for="birth_date" class="form-label">Tanggal Lahir</label>
-             <input type="date" class="form-control" name="birth_date" id="birth_date" required>
-         </div>
-
-         <div class="mb-3">
-             <label for="gender" class="form-label">Jenis Kelamin</label>
-             <select class="form-select" id="gender" name="gender" required>
-                 <option value="0">Perempuan</option>
-                 <option value="1">Laki - Laki</option>
-             </select>
+             <label for="content" class="form-label">Isi Aspirasi</label>
+             <textarea name="content" class="form-control" id="content" cols="30" rows="10">{{ old('content_response') }}</textarea>
          </div>
 
          <div class="mb-3">
