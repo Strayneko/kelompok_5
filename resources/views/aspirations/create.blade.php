ed77f072a057fd7707eef6f31aaa59832193c3dd
Nora Neko <bayurendi10@gmail.com>
2022-12-24 14:11:15 +0700

    add create aspiration:

--- resources/views/aspirations/create.blade.php
+++ resources/views/aspirations/create.blade.php
@@ -2,8 +2,9 @@
  @section('title', 'Tambah Aspirasi')
  @section('content')
      <h1 class="my-4">Form aspirasi</h1>
-     <form method="post" class="col-md-4" enctype="multipart/form-data" id="aspiration_form">
+     <form method="post" class="col-md-4" enctype="multipart/form-data" action="#" id="aspiration_form">
          @csrf
+         <input type="hidden" id="user_id" value="{{ Auth::user()->id }}">
 
          <div class="mb-3">
              <label for="title" class="form-label">Judul</label>
@@ -25,30 +26,33 @@
      </form>
      <script>
          document.addEventListener('DOMContentLoaded', () => {
-         $("#aspiration_form").on('submit', (e) => {
-             e.preventDefault();
-             const title = $('#title').val();
-             const content = $('#content').text()
-             const image = $("#image").prop('files')[0]
-             const fd = new FormData();
-             fd.append('title', title);
-             fd.append('image', image);
-             fd.append('content', content);
-             fetch('http://127.0.0.1:8000/api/aspiration/create', {
-                 method: 'POST',
-                 body: fd
-             }).then(res => res.json()).then(res => {
-                 if (!res.status) {
-                     alert(res.message);
-                     return;
-                 }
-                 alert(res.message)
-                 location.href = "http://127.0.0.1:8000/aspiration/create"
+             $("#aspiration_form").on('submit', (e) => {
+                 e.preventDefault();
+                 const title = $('#title').val();
+                 const content = $('#content').val()
+                 const user_id = $('#user_id').val()
+                 const image = $("#image").prop('files')[0]
+                 const fd = new FormData();
+                 fd.append('title', title);
+                 fd.append('image', image);
+                 fd.append('user_id', user_id);
+                 fd.append('content', content);
+                 fetch('http://127.0.0.1:8000/api/aspiration/create', {
+                         method: 'POST',
+                         body: fd
+                     })
+                     .then(res => res.json())
+                     .then(res => {
+                         if (!res.status) {
+                             alert(res.message);
+                             return;
+                         }
+                         alert(res.message)
+                         location.href = "http://127.0.0.1:8000/aspiration/create"
+                     })
+
              })
 
-         })
-
-         })
          })
      </script>
  @endsection
