62bdff5451836bd1ea991dff06aa56fd04ae0ffe
Nora Neko <bayurendi10@gmail.com>
2022-12-24 09:21:05 +0700

    (+) add protected filed to models

--- app/Models/Aspiration.php
+++ app/Models/Aspiration.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Aspiration extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
 }
