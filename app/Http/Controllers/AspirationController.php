893de9e239c4e66f75a5594cfd63455f74945b45
tegarp00 <tegarp00@gmail.com>
2022-12-24 10:23:46 +0700

    lupa commit

--- app/Http/Controllers/AspirationController.php
+++ app/Http/Controllers/AspirationController.php
@@ -4,17 +4,31 @@ namespace App\Http\Controllers\backend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Aspiration;
 
 class AspirationController extends Controller
 {
-    //TODO: show add aspiration form
-    public function create(Request $request)
+    //TODO: show store aspiration
+    public function store(Request $request)
     {
+
+        $payload = [
+            'title' => $request->input("title"),
+            'content_response' => $request->input("content_response"),
+            'file' => $request->file->store("aspirations", "public")
+        ]; 
+
+        $aspirasi = Aspiration::query()->create($payload);
+        return view('aspirations.index', compact('aspirasi'));
+
     }
 
     // TODO: show all aspirations data
     public function index()
     {
+        $aspirasi = Aspiration::query()->get();
+
+        return view('aspirations.index', compact('aspirasi'));
     }
 
     // TODO: show update form
@@ -26,14 +40,37 @@ class AspirationController extends Controller
     // TODO: update aspiration data by the given id
     public function update(Request $request, $id)
     {
+        $aspirasi = Aspiration::find($id);
+        $aspirasi->title = $request->input('title');
+        $aspirasi->content_response = $request->input('content_response');
+        $aspirasi->status = $request->input('status');
+
+        // cek input image
+        if($request->file("image")) {
+            $file = $request->file("image");
+            $filename = $file->hashName();
+            $file->move("aspirations", $filename);
+            $path = "/aspirations/".$filename;
+
+            $aspirasi->file = $path;
+        }
+
+        $aspirasi->update();
+
+        return view('aspirations.index');
     }
     // TODO: show detail aspiration by the given id
     public function show($id)
     {
+        $aspirasi = Aspiration::find($id);
+
+        return view('', compact('aspirasi'));
     }
 
     // TODO: delete specific aspiration data by id
     public function destroy($id)
     {
+        Aspiration::query()->where("id", $id)->delete();
+        return redirect('aspirations.index');
     }
 }
