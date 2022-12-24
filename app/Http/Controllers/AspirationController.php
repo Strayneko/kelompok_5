358fbba1bfe3804cc73005e1a05426711857d850
Nora Neko <bayurendi10@gmail.com>
2022-12-24 09:26:30 +0700

    (+) add base functions

--- app/Http/Controllers/AspirationController.php
+++ app/Http/Controllers/AspirationController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class AspirationController extends Controller
 {
-    //
+    //TODO: show add aspiration form
+    public function create(Request $request)
+    {
+    }
+
+    // TODO: show all aspirations data
+    public function index()
+    {
+    }
+
+    // TODO: show update form
+    public function edit(Request $request, $id)
+    {
+    }
+
+
+    // TODO: update aspiration data by the given id
+    public function update(Request $request, $id)
+    {
+    }
+    // TODO: show detail aspiration by the given id
+    public function show($id)
+    {
+    }
+
+    // TODO: delete specific aspiration data by id
+    public function destroy($id)
+    {
+    }
 }
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,4 +7,23 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
+
+    // TODO: show login form
+    public function login()
+    {
+    }
+
+    // TODO: authenticate user
+    public function authenticate(Request $request)
+    {
+    }
+
+    // TODO: show registration form
+    public function register()
+    {
+    }
+    // TODO: register user 
+    public function registration(Request $request)
+    {
+    }
 }
