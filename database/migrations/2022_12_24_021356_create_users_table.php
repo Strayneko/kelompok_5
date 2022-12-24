66a0b84465d3ac290feed94c45e743d8475c5bf3
Nora Neko <bayurendi10@gmail.com>
2022-12-24 09:19:48 +0700

    :sparkle: add migrations

---
+++ database/migrations/2022_12_24_021356_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('role_id');
+            $table->string('name', 50);
+            $table->string('email', 100);
+            $table->string('password', 255);
+            $table->text('image');
+            $table->tinyInteger('gender');
+            $table->date('birth_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
