b9735374265be6daa02ec2638bac19a4b689e2ce
Nora Neko <bayurendi10@gmail.com>
2022-12-24 12:34:44 +0700

    fix registration bug

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::post('/register/a', [AuthController::class, 'registration']);
 Route::prefix('auth')
     ->controller(AuthController::class)
     ->group(function () {
